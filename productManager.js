215855b7d542140a8e13efbf6594e5b0db5ae5e4
Amelia <meli_gallegos@yahoo.com.ar>
2024-03-07 00:58:34 -0300

    correccionesSegundoDesafío

--- productManager.js
+++ productManager.js
@@ -1,23 +1,21 @@
 //Importo File System
 const fs = require("fs");
 
-//Creo constante para guardar path
-const guardarPath = "./array-products.json";
-
 //Creo clase con su constructor que recibe los parámetros solicitados
 class ProductManager {
   //Variable para generar id autoincrementable
   static id = 0;
 
-  constructor(products = [], path) {
-    this.products = products;
-    this.path = path;
+  constructor() {
+    this.path = "./array-product.json";
+    this.products = [];
   }
 
+  
   //Creo método para ingresar productos al array vacío
   async addProduct(product) {
     const { title, description, price, thumbnail, code, stock } = product;
-
+    
     //Validación para buscar código de producto
     if (this.products.some((el) => el.code === code)) {
       console.log(
@@ -42,39 +40,47 @@ class ProductManager {
     }
 
     //Se agrega producto al array, con sus propiedades y id autoincrementable
-    const productsNew = this.products.push({
-      ...product,
-      id: ++ProductManager.id,
-    });
-    console.log(productsNew);
+    else {
+      const productsNew = this.products.push({
+        ...product,
+        id: ++ProductManager.id,
+      });
+      console.log(productsNew);
+    }
+    await this.crearJson();
+  }
+
+  //Método para mostrar array vacío
+  async getProductsVacío() {
+    return this.products;
   }
 
   //Creación/guardado del archivo con promesa
   async crearJson() {
     try {
       await fs.promises.writeFile(
-        guardarPath,
+        this.path,
         JSON.stringify(this.products, null, 2)
       );
     } catch (error) {
-      console.log(error);
+      console.log(mensaje.error);
+      throw error;
     }
   }
 
-  // //Lectura del archivo con promesa
+  //Lectura del archivo con promesa
   async leerJson() {
     try {
-      const respuesta = await fs.promises.readFile(guardarPath, "utf-8");
-      const respuesta2 = JSON.parse(respuesta);
-      console.log(respuesta2);
-      return
+      const respuesta1 = await fs.promises.readFile(this.path, "utf-8");
+      const respuesta2 = JSON.parse(respuesta1);
+      return respuesta2;
     } catch (error) {
       console.log(error);
       throw error;
     }
   }
 
-  // //Método para mostrar el array de productos
+  //Método para mostrar el array de productos
   async getProducts() {
     try {
       const resultado = await this.leerJson();
@@ -87,149 +93,141 @@ class ProductManager {
     }
   }
 
-//   //Método para buscar id de producto
-//   async getProductById(id) {
-//     try {
-//       const resultado = await this.leerJson();
+  //Método para buscar id de producto
+  async getProductById(id) {
+    try {
+      const resultado = await this.leerJson();
+      const buscarId = resultado.find((el) => el.id === id);
 
-//       const buscarId = resultado.find((el) => el.id === id);
+      if (buscarId) {
+        console.log(
+          `Búsqueda por id\nEl id: ${id} pertenece al producto: `,
+          buscarId
+        );
+      } else {
+        console.log(
+          `Búsqueda por id\nEl id: ${id} no fue encontrado. Puede seguir buscando.`
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 
-//       if (buscarId) {
-//         console.log(
-//           `Búsqueda por id\nEl id: ${id} pertenece al producto: `,
-//           buscarId
-//         );
-//       } else {
-//         console.log(
-//           `Búsqueda por id\nEl id: ${id} no fue encontrado. Puede seguir buscando.`
-//           // `Búsqueda por id\nEl id: ${id} no se encuentra en la lista. Puede continuar buscando.`,
-//         );
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       throw error;
-//     }
-//   }
-// }
+  //Método para reemplazar al producto que coincida con el id ingresado
+  async updateProduct(id, updateProduct) {
+    try {
+      const resultado = await this.leerJson();
 
-//Método para reemplazar producto
-// async updateProduct(id) {
-//   const resultado = await this.leerJson();
-
-//   if (resultado.find((el) => el.id === id)) {
-//     let resultadoNuevo = await fs.promises.writeFile(
-//       guardarPath,
-//       JSON.stringify(resultado[id], null, 2));
-//       console.log (
-//         "La lista actualizada es la siguiente : " + JSON.stringify(resultadoNuevo, null, 2)
-//       );
-//       return
-//   } else {
-//     console.log(
-//       "Los productos ingresados son: " + JSON.stringify(resultado, null, 2)
-//     );
-//     return
-//   }
-// }
-// }
+      const actualizarProd = resultado.map((product) => {
+        if (product.id === id) {
+          return { ...updateProduct, id: id };
+        }
+        return product;
+      });
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(actualizarProd, null, 2)
+      );
+      console.log("El producto con el id: " + id + " fue actualizado.");
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  //Método para eliminar al producto que coincida con el id ingresado
+  async deleteProduct  (id) {
+    try {
+      const resultado = await this.leerJson();
+      const resultadoNuevo = resultado.filter(product => product.id !== id);
+  
+      await fs.promises.writeFile(this.path, JSON.stringify(resultadoNuevo, null, 2));
+      console.log("El producto con el id: " + id + " fue eliminado");
+  
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
+
 async function array() {
-  // //Instancia para mostrar array vacío
-  // const test = new ProductManager();
+  //Instancia para mostrar array vacío
+  const test = new ProductManager();
+  const arrayVacio = await test.getProductsVacío();
+  console.log(arrayVacio);
 
   //Instancias para agregar productos
   const productTest = new ProductManager();
-  await productTest.leerJson();
 
+  await productTest.addProduct({
+    title: "Producto Prueba",
+    description: "Este es un producto prueba",
+    price: 200,
+    thumbnail: "Sin Imagen",
+    code: "abc123",
+    stock: 25,
+  });
 
-  await productTest.addProduct(
-    {
-      title: "Producto Prueba",
-      description: "Este es un producto prueba",
-      price: 200,
-      thumbnail: "Sin Imagen",
-      code: "abc123",
-      stock: 25,
-    },
-    // "./array-products.json"
-  );
+  await productTest.addProduct({
+    title: "Oreja de Srhek",
+    description: "Suculenta",
+    price: 2500,
+    thumbnail: "Sin Imagen",
+    code: "abc1234",
+    stock: 25,
+  });
 
-  await productTest.addProduct(
-    {
-      title: "Oreja de Srhek",
-      description: "Suculenta",
-      price: 2500,
-      thumbnail: "Sin Imagen",
-      code: "abc1234",
-      stock: 25,
-    },
-    // "./array-products.json"
-  );
-
-  await productTest.addProduct(
-    {
-      title: "Cola de Burro",
-      description: "Suculenta Colgante",
-      price: 3000,
-      thumbnail: "Sin Imagen",
-      code: "abc1235",
-      stock: 25,
-    },
-    // "./array-products.json"
-  );
-
-  // //Producto para reemplazar al producto que coincida con su id
-  // productTest.addProduct(
-  //   {
-  //     title: "Aloe Vera",
-  //     description: "Suculenta",
-  //     price: 2500,
-  //     thumbnail: "Sin Imagen",
-  //     code: "abc1237",
-  //     stock: 25,
-  //   },
-  //   "./array-products.json"
-  // );
+  await productTest.addProduct({
+    title: "Cola de Burro",
+    description: "Suculenta Colgante",
+    price: 3000,
+    thumbnail: "Sin Imagen",
+    code: "abc1235",
+    stock: 25,
+  });
 
   //Producto para validar code repetido
-  await productTest.addProduct(
-    {
-      title: "Arbol de Jade",
-      description: "Suculenta",
-      price: 2000,
-      thumbnail: "Sin Imagen",
-      code: "abc123",
-      stock: 25,
-    },
-    // "./array-products.json"
-  );
+  await productTest.addProduct({
+    title: "Arbol de Jade",
+    description: "Suculenta",
+    price: 2000,
+    thumbnail: "Sin Imagen",
+    code: "abc123",
+    stock: 25,
+  });
 
   //Producto para validar campo incompleto
- await productTest.addProduct(
-    {
-      title: "",
-      description: "Suculenta Colgante",
-      price: 3000,
-      thumbnail: "Sin Imagen",
-      code: "abc1236",
-      stock: 25,
-    },
-    // "./array-products.json"
-  );
+  await productTest.addProduct({
+    title: "",
+    description: "Suculenta Colgante",
+    price: 3000,
+    thumbnail: "Sin Imagen",
+    code: "abc1236",
+    stock: 25,
+  });
+  await productTest.leerJson();
 
-  // //Invoco instancia que mostrar array vacío
-  // const arrayVacio = await test.getProducts();
-  // console.log(arrayVacio);
-
-  // //Invoco instancia para mostrar productos agregados
+  //Invoco instancia para mostrar productos agregados
   const arrayProducts = await productTest.getProducts();
   console.log(arrayProducts);
 
   //Invoco instancias para hacer búsqueda de producto por id
-  // productTest.getProductById(8);
-  // productTest.getProductById(2);
+  productTest.getProductById(8);
+  productTest.getProductById(2);
 
   //Invoco instancia para reemplazar producto
-  // productTest.updateProduct(3);
+  await productTest.updateProduct(3, {
+    title: "Aloe Vera",
+    description: "Suculenta",
+    price: 2500,
+    thumbnail: "Sin Imagen",
+    code: "abc1237",
+    stock: 25,
+  });
+
+  //Invoco instancia para eliminar producto
+   productTest.deleteProduct(1);
 }
 array();
